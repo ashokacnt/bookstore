c04fda1affd950c2545ee471690e9d2832586d1c
Ashok <javatrainm@gmail.com>
2026-09-21 09:18:37 +0530

    add openapi swagger documentation.

---
+++ src/main/java/com/cg/config/OpenApiConfig.java
@@ -0,0 +1,13 @@
+package com.cg.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.*;
+
+@Configuration
+public class OpenApiConfig {
+    @Bean
+    OpenAPI openAPI() {
+        return new OpenAPI().info(new Info().title("Bookstore V6 API").version("1.0.0").description("Online bookstore using Spring Boot, Basic Authentication and modular architecture."));
+    }
+}
