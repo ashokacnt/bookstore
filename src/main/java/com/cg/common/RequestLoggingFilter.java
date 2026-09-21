d46a518f862b5f06019f717d7ed9bb8baf2a62dd
Ashok <javatrainm@gmail.com>
2026-09-21 08:13:11 +0530

    add logging and request correlation

--- src/main/java/com/cg/common/RequestLoggingFilter.java
+++ src/main/java/com/cg/common/RequestLoggingFilter.java
@@ -1,8 +1,11 @@
 package com.cg.common;
 
-import jakarta.servlet.*;
-import jakarta.servlet.http.*;
-import org.slf4j.*;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.slf4j.MDC;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
